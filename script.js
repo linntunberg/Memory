9916c5b1f253a6ed7e0f88678e50713f02864646
linntunberg <31646491+linntunberg@users.noreply.github.com>
2018-01-30 16:58:44 +0100

    change

---
+++ script.js
@@ -0,0 +1,80 @@
+<script>
+function changeColor() {
+    document.getElementById("one").style.backgroundColor = "lightblue";
+}
+
+
+function changeColor2() {
+    document.getElementById("two").style.backgroundColor = "lightblue";
+}
+
+
+function changeColor3() {
+    document.getElementById("three").style.backgroundColor = "pink";
+}
+
+
+function changeColor4() {
+    document.getElementById("four").style.backgroundColor = "pink";
+}
+
+
+function changeColor5() {
+    document.getElementById("five").style.backgroundColor = "green";
+}
+
+
+function changeColor6() {
+    document.getElementById("six").style.backgroundColor = "green";
+}
+
+
+function changeColor7() {
+    document.getElementById("seven").style.backgroundColor = "yellow";
+}
+
+
+function changeColor8() {
+    document.getElementById("eight").style.backgroundColor = "yellow";
+}
+
+
+function changeColor9() {
+    document.getElementById("nine").style.backgroundColor = "red";
+}
+
+
+function changeColor10() {
+    document.getElementById("ten").style.backgroundColor = "red";
+}
+
+
+function changeColor11() {
+    document.getElementById("eleven").style.backgroundColor = "grey";
+}
+
+
+function changeColor12() {
+    document.getElementById("twelve").style.backgroundColor = "grey";
+}
+
+
+function changeColor13() {
+    document.getElementById("thirteen").style.backgroundColor = "orange";
+}
+
+
+function changeColor14() {
+    document.getElementById("fourteen").style.backgroundColor = "orange";
+}
+
+
+function changeColor15() {
+    document.getElementById("fifteen").style.backgroundColor = "purple";
+}
+
+
+function changeColor16() {
+    document.getElementById("sixteen").style.backgroundColor = "purple";
+}
+</script>

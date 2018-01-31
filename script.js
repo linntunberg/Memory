ef7199cba49907ee4f5006c8936618ecfeb426a2
linntunberg <31646491+linntunberg@users.noreply.github.com>
2018-01-31 22:37:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,80 +1,55 @@
-<script>
-function changeColor() {
-    document.getElementById("one").style.backgroundColor = "lightblue";
-}
+var clicks = 0;
+var firstchoice;
+var secondchoice;
+
+var match = 0;
+var backcard = "back.jpeg";
+
+var image = [];
+image[0] = 'nicolas1.jpg';
+image[1] = 'nicolas2.jpg';
+image[2] = 'nicolas3.jpg';
+image[3] = 'nicolas4.jpg';
+image[4] = 'nicolas5.jpg';
+image[5] = 'nicolas6.jpg';
+image[6] = 'nicolas7.jpg';
+image[7] = 'nicolas8.jpg';
+image[8] = 'nicolas1.jpg';
+image[9] = 'nicolas2.jpg';
+image[10] = 'nicolas3.jpg';
+image[11] = 'nicolas4.jpg';
+image[12] = 'nicolas5.jpg';
+image[13] = 'nicolas6.jpg';
+image[14] = 'nicolas7.jpg';
+image[15] = 'nicolas8.jpg';
 
 
-function changeColor2() {
-    document.getElementById("two").style.backgroundColor = "lightblue";
-}
+function choose(card) {
+				if (clicks == 2) {
+						return;
+				}
+				if (clicks == 0) {
+						firstchoice = card;
+						document.images[card].src = image[card];
+						clicks = 1;
+				} else {
+						clicks = 2;
+						secondchoice = card;
+						document.images[card].src = image[card];
+						timer = setInterval("check()", 1000);
+				}
+		}
 
 
-function changeColor3() {
-    document.getElementById("three").style.backgroundColor = "pink";
-}
+		function check() {
+				clearInterval(timer); 
+				clicks = 0;
+				if (image[secondchoice] == image[firstchoice]) {
+						match++;
 
-
-function changeColor4() {
-    document.getElementById("four").style.backgroundColor = "pink";
-}
-
-
-function changeColor5() {
-    document.getElementById("five").style.backgroundColor = "green";
-}
-
-
-function changeColor6() {
-    document.getElementById("six").style.backgroundColor = "green";
-}
-
-
-function changeColor7() {
-    document.getElementById("seven").style.backgroundColor = "yellow";
-}
-
-
-function changeColor8() {
-    document.getElementById("eight").style.backgroundColor = "yellow";
-}
-
-
-function changeColor9() {
-    document.getElementById("nine").style.backgroundColor = "red";
-}
-
-
-function changeColor10() {
-    document.getElementById("ten").style.backgroundColor = "red";
-}
-
-
-function changeColor11() {
-    document.getElementById("eleven").style.backgroundColor = "grey";
-}
-
-
-function changeColor12() {
-    document.getElementById("twelve").style.backgroundColor = "grey";
-}
-
-
-function changeColor13() {
-    document.getElementById("thirteen").style.backgroundColor = "orange";
-}
-
-
-function changeColor14() {
-    document.getElementById("fourteen").style.backgroundColor = "orange";
-}
-
-
-function changeColor15() {
-    document.getElementById("fifteen").style.backgroundColor = "purple";
-}
-
-
-function changeColor16() {
-    document.getElementById("sixteen").style.backgroundColor = "purple";
-}
-</script>
+				} else {
+						document.images[firstchoice].src = backcard;
+						document.images[secondchoice].src = backcard;
+						return;
+				}
+		}

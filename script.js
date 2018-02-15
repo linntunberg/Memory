4fd6b9ab021b026338d945d2bf7154e89a140c39
linntunberg <linn.tunberg@gmail.com>
2018-02-15 13:13:51 +0100

    Changes

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var clicks = 0;
-var firstchoice;
-var secondchoice;
+let clicks = 0;
+let firstchoice;
 
-var match = 0;
+let match = 0;
 
-var image = ['nicolas1.jpg', 'nicolas1.jpg', 'nicolas2.jpg', 'nicolas2.jpg', 'nicolas3.jpg', 'nicolas3.jpg', 'nicolas4.jpg', 'nicolas4.jpg', 'nicolas5.jpg', 'nicolas5.jpg', 'nicolas6.jpg', 'nicolas6.jpg', 'nicolas7.jpg', 'nicolas7.jpg', 'nicolas8.jpg', 'nicolas8.jpg'];
+let image = ['nicolas1.jpg', 'nicolas1.jpg', 'nicolas2.jpg', 'nicolas2.jpg', 'nicolas3.jpg', 'nicolas3.jpg', 'nicolas4.jpg', 'nicolas4.jpg', 'nicolas5.jpg', 'nicolas5.jpg', 'nicolas6.jpg', 'nicolas6.jpg', 'nicolas7.jpg', 'nicolas7.jpg', 'nicolas8.jpg', 'nicolas8.jpg'];
 
 function shuffle(a)
 {
@@ -19,59 +18,6 @@ function shuffle(a)
 }
 image = shuffle(image);
 
-
-function choose(card) {
-	if (clicks == 2) {
-		return;
-	}
-	if (clicks == 0) {
-		firstchoice = card;
-		document.images[card].src = image[card];
-		clicks = 1;
-	} else {
-		clicks = 2;
-		secondchoice = card;
-		document.images[card].src = image[card];
-		timer = setInterval("check()", 1000);
-	}
-}
-
-
-
-function check() {
-	clearInterval(timer);
-	clicks = 0;
-	if (image[secondchoice] == image[firstchoice]) {
-		match++;
-
-	} else {
-		document.images[firstchoice].src = backcard;
-		document.images[secondchoice].src = backcard;
-		return;
-	}
-}
-
-function checkCard(card);
-
-
-
-if(!firstchoice)
-
-
-
-
-
-
-
-
-
-document.getElementById("button").addEventListener("click", function() {
-	location.reload();
-});
-
-
-
-
 image.forEach(function(img) {
 	let card= document.createElement('div'); //creates element div
 	card.classList.add('card'); //adds a the class named "card" to the element "card", adding this to the first card
@@ -82,6 +28,67 @@ image.forEach(function(img) {
 	{
 		let card = event.target;
 		card.classList.add('show') //adds the class "show"
+		choose(card);
 	});
 	document.body.querySelector('.content').appendChild(card); //adds content to the body of the document
+});
+
+function choose(card) {
+	if(firstchoice == null) {
+		firstchoice = card;
+	}
+	else {
+		if (firstchoice.style.backgroundImage == card.style.backgroundImage) {
+			firstchoice.remove();
+			card.remove();
+			firstchoice = null;
+		}
+		else {
+			setTimeout(()=>{
+				card.classList.remove('show');
+				firstchoice.classList.remove('show');
+				firstchoice = null;
+			},500);
+
+		}
+	}
+}
+
+
+	// if (clicks == 2) {
+	// 	return;
+	// }
+	// if (clicks == 0) {
+	// 	firstchoice = card;
+	// 	document.images[card].src = image[card];
+	// 	clicks = 1;
+	// } else {
+	// 	clicks = 2;
+	// 	secondchoice = card;
+	// 	document.images[card].src = image[card];
+	// 	timer = setInterval("check()", 1000);
+
+//
+//
+//
+// function check() {
+// 	clearInterval(timer);
+// 	clicks = 0;
+// 	if (image[secondchoice] == image[firstchoice]) {
+// 		match++;
+//
+// 	} else {
+// 		document.images[firstchoice].src = backcard;
+// 		document.images[secondchoice].src = backcard;
+// 		return;
+// 	}
+//
+
+
+
+
+
+
+document.getElementById("button").addEventListener("click", function() {
+	location.reload();
 });
